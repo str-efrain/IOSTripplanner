1e71fa73b6050c822d54079193834ac8d2a3f198
efrain mouton <efrain.mouton@student.vives.be>
2025-09-30 09:44:49 +0200

    Initial Commit

---
+++ tripPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tripPlanner
+//
+//  Created by Efrain Mouton on 30/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tripPlanner/tripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripPlannerApp.swift
+//  tripPlanner
+//
+//  Created by Efrain Mouton on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct tripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
