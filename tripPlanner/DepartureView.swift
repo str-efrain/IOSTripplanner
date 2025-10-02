59787ad3c0ce90212a9076e97c67e73357941ba7
efrain mouton <efrain.mouton@student.vives.be>
2025-10-02 12:38:47 +0200

    Afwerking van opdracht met hardcoded info

--- tripPlanner/DepartureView.swift
+++ tripPlanner/DepartureView.swift
@@ -10,31 +10,52 @@ import SwiftUI
 struct DepartureView: View {
     var body: some View {
         VStack {
-            
             HStack {
+                Spacer()
                 VStack {
                     Text("BRU").bold().foregroundStyle(.tint).font(.title)
                     Text("Brussels").foregroundStyle(.secondary)
                     Text("8:15")
                 }
-                Image(systemName: "airplane").foregroundStyle(.secondary)
+                Spacer()
+                Image(systemName: "airplane").resizable().frame(width: 50, height: 50).foregroundStyle(.secondary)
+                Spacer()
                 VStack {
                     Text("BCN").bold().foregroundStyle(.tint).font(.title)
                     Text("Barcelona").foregroundStyle(.secondary)
                     Text("11:15")
                 }
-            }
+                Spacer()
+            }.padding(.top, 55)
             Grid {
+                Divider().overlay(.white)
                 GridRow {
-                    Divider().overlay(.tint)
-
                     Text("flight")
                     Text("gate")
                     Text("seat")
+                }.padding(.top)
+                GridRow {
+                    Text("SN23A")
+                    Text("B23")
+                    Text("27A")
+                }.bold().padding(.bottom)
+            }.background(.tint, in: RoundedRectangle(cornerRadius: 12)).foregroundStyle(.white).padding(.top).padding()
+            HStack {
+                Spacer()
+                VStack {
+                    Text("Passenger").foregroundStyle(.secondary)
+                    Text("Dirk Hostens").padding(.bottom, 5)
+                    Text("Class").foregroundStyle(.secondary)
+                    Text("Business").padding(.bottom, 5)
+                    Text("Flight date").foregroundStyle(.secondary)
+                    Text("27/08/2024").padding(.bottom, 5)
                 }
+                Spacer()
+                Image(systemName: "person.crop.artframe").resizable().frame(width: 115, height: 160).foregroundStyle(.secondary).padding()
+                Spacer()
             }
-            
         }
+        Spacer()
     }
 }
 
--- tripPlanner/ReturnView.swift
+++ tripPlanner/ReturnView.swift
@@ -11,19 +11,53 @@ struct ReturnView: View {
     var body: some View {
         VStack {
             HStack {
+                Spacer()
                 VStack {
                     Text("BRU").bold().foregroundStyle(.tint).font(.title)
                     Text("Brussels").foregroundStyle(.secondary)
                     Text("8:15")
                 }
-                Image(systemName: "airplane").foregroundStyle(.secondary)
+                Spacer()
+                Image(systemName: "airplane").resizable().frame(width: 50, height: 50).foregroundStyle(.secondary)
+                Spacer()
                 VStack {
                     Text("BCN").bold().foregroundStyle(.tint).font(.title)
                     Text("Barcelona").foregroundStyle(.secondary)
                     Text("11:15")
                 }
+                Spacer()
+
+            }.padding(.top, 55)
+
+            Grid {
+                Divider().overlay(.white)
+                GridRow {
+                    Text("flight")
+                    Text("gate")
+                    Text("seat")
+                }.padding(.top)
+                GridRow {
+                    Text("SN205")
+                    Text("XD-30")
+                    Text("17C")
+                }.bold().padding(.bottom)
+            }.background(.tint, in: RoundedRectangle(cornerRadius: 12)).foregroundStyle(.white).padding(.top).padding()
+            HStack {
+                Spacer()
+                VStack {
+                    Text("Passenger").foregroundStyle(.secondary)
+                    Text("Dirk Hostens").padding(.bottom, 5)
+                    Text("Class").foregroundStyle(.secondary)
+                    Text("Business").padding(.bottom, 5)
+                    Text("Flight date").foregroundStyle(.secondary)
+                    Text("1/09/2024").padding(.bottom, 5)
+                }
+                Spacer()
+                Image(systemName: "person.crop.artframe").resizable().frame(width: 115, height: 160).foregroundStyle(.secondary).padding()
+                Spacer()
             }
         }
+        Spacer()
     }
 }
 
